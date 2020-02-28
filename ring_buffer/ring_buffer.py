e890c6a7086a1fc2d5a1742e81b52da10ceef58d
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-02-28 10:16:03 -0600

    RingBuffer append method is adding items, but not sure if it is currently overwriting the oldest one yet.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,7 +8,12 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # If the ring is full, remove oldest, add new
+        if self.capacity == self.storage.length:
+            self.storage.remove_from_tail()
+            self.storage.add_to_head(item)
+        else:
+            self.storage.add_to_head(item)
 
     def get(self):
         # Note:  This is the only [] allowed
@@ -30,3 +35,13 @@ class ArrayRingBuffer:
 
     def get(self):
         pass
+
+buffer = RingBuffer(5)
+print(buffer.storage.length)
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+buffer.append('d')
+buffer.append('e')
+buffer.append('f')
+print(buffer.storage.length)
